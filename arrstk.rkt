7fd59b015c09e939f58ff38019b2f093265977a4
Spencer <snsallay@gmail.com>
2015-05-25 22:11:43 -0400

    push files to repo

---
+++ arrstk.rkt
@@ -0,0 +1,63 @@
+#lang racket/base
+(require racket/list)
+
+; successor to arrstk, this language will look like any other stack language
+; for a little bit.
+
+(struct v (val type))
+(define (push stk elt) (append stk (list elt)))
+(define (pop stk) (car (reverse stk)))
+(define (ret-pop stk) (reverse (cdr (reverse stk))))
+
+(define funs* (list (list "+" (list "#Int" "#Int") (list "#Int"))))
+
+(define (write-spec ls) 
+  (if (list? ls) (begin (display "(") (map write-spec ls) (display ")"))
+      (cond [(v? ls) (begin (display "(v ") (write-spec (v-val ls)) (write-spec (v-type ls)) (display ")"))] 
+            [else (write ls)])))
+
+(define (string-split-spec str)
+  (filter (λ (x) (not (empty? (string->list x)))) (splt str '())))
+  ;(splt str '()))
+(define (splt str lst)
+  (if (empty? (string->list str)) lst
+      (splt (cadr (tok (string->list str) '())) (append lst (list (car (tok (string->list str) '())))))))
+
+(define (tok str lst)
+  (if (empty? str) (list (list->string lst) "")
+    (let ([c (car str)])
+      (if (and (not (empty? lst)) (equal? (car lst) #\"))
+          (if (equal? c #\") (list (list->string (append lst (list c))) (list->string (cdr str)))
+              (tok (cdr str) (append lst (list c))))
+          (if (or (char-whitespace? c)) (if (empty? lst) (tok (cdr str) lst) (list (list->string lst) (list->string str)))
+              (tok (cdr str) (append lst (list c))))))))
+
+(define (strcar s) (car (string->list s)))
+
+(define (lex l)
+  (cond [(or (char-numeric? (strcar l)) (char=? (strcar l) #\.)) (v l "#Int")]
+        [(char=? (strcar l) #\") (v l "#String")]
+        [(char=? (strcar l) #\#) (v l "#Type")]
+        [else (v l "#Sym")]))
+
+(define (fexists? s fns) (member s (map car fns)))
+(define (get-f s fns) (findf (λ (x) (equal? (car x) s)) fns))
+(define (call-fun f stk)
+  (let ([sub (list (pop (ret-pop stk)) (pop stk))])
+    (if (not (equal? (map v-type sub) (second f))) (displayln "ERROR: type mismatch.")
+        (begin ; (do the C stuff)
+               (append (take stk (- (length stk) (length sub))) (map (λ (x) (v (list (car f) sub) x)) (third f)))))))
+
+(define (push~ stk s)
+  (cond [(equal? (v-type s) "#Sym") (if (fexists? (v-val s) funs*) (call-fun (get-f (v-val s) funs*) stk)
+                                        (push stk s))]
+        [else (push stk s)]))
+
+(define (process stk n)
+  (if (empty? stk) n (process (cdr stk) (push~ n (car stk)))))
+
+(define (main)
+  (write-spec (process (map lex (string-split-spec (read-line))) '()))
+  (main))
+
+(main)
